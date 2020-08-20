26ca2cf0704e518d5a68f3a594b829779b74bdf1
Hermann <blumh@ethz.ch>
2020-08-20 23:55:33 +0200

    add orcid api

---
+++ plugins/gatsby-source-orcid/gatsby-node.js
@@ -0,0 +1,31 @@
+const fetch = require(`node-fetch`)
+
+exports.sourceNodes = async ({
+  actions,
+  createNodeId,
+  createContentDigest,
+}) => {
+  const { createNode } = actions
+  // Download data from a remote API.
+  const data = await fetch(
+    "https://pub.orcid.org/v2.1/0000-0002-1713-7877/works",
+    { headers: {Accept: "application/json"} }
+  )
+  const dataResult = await data.json();
+  dataResult.group.forEach(item => {
+    createNode({
+      id: createNodeId(item['work-summary'][0].path),
+      parent: null,
+      childen: [],
+      external_ids: item['external-ids'],
+      summary: item['work-summary'][0],
+      internal: {
+        type: "OrcidRecord",
+        content: JSON.stringify(item),
+        contentDigest: createContentDigest(item),
+      },
+    })
+  })
+
+  return
+}
