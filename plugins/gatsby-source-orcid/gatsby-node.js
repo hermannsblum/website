fd979b261c96cfe58389b7d56cf9d572dcab68ad
Hermann <blumh@ethz.ch>
2020-08-21 12:25:20 +0200

    organized data structure

--- plugins/gatsby-source-orcid/gatsby-node.js
+++ plugins/gatsby-source-orcid/gatsby-node.js
@@ -9,18 +9,41 @@ exports.sourceNodes = async ({
   // Download data from a remote API.
   const data = await fetch(
     "https://pub.orcid.org/v2.1/0000-0002-1713-7877/works",
-    { headers: {Accept: "application/json"} }
+    { headers: { Accept: "application/json" } }
   )
-  const dataResult = await data.json();
+  const dataResult = await data.json()
   dataResult.group.forEach(item => {
+    const paperLinkIds = []
+    item["external-ids"]["external-id"].forEach(externalId => {
+      const paperLinkId = createNodeId(externalId["external-id-url"].value)
+      paperLinkIds.push(paperLinkId)
+      createNode({
+        id: paperLinkId,
+        parent: null,
+        children: [],
+        url: externalId["external-id-url"].value,
+        identifier: externalId["external-id-value"],
+        internal: {
+          type: externalId["external-id-type"],
+          content: JSON.stringify(externalId),
+          contentDigest: createContentDigest(externalId),
+        }
+      })
+    })
+    const summary = item["work-summary"][0]
     createNode({
-      id: createNodeId(item['work-summary'][0].path),
+      id: createNodeId(summary.title.title.value),
       parent: null,
-      childen: [],
-      external_ids: item['external-ids'],
-      summary: item['work-summary'][0],
+      children: paperLinkIds,
+      title: summary.title.title.value,
+      date: {
+        day: summary["publication-date"].day.value,
+        month: summary["publication-date"].month.value,
+        year: summary["publication-date"].year.value,
+      },
+      summary: summary,
       internal: {
-        type: "OrcidRecord",
+        type: "Paper",
         content: JSON.stringify(item),
         contentDigest: createContentDigest(item),
       },
