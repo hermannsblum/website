75c7b7f09d85d77d46e1f4e4d12029542eb82f6a
Hermann <blumh@ethz.ch>
2021-05-13 22:00:41 +0200

    add robot illustrations

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,14 +8,8 @@ export default function Layout({ children }) {
       <Helmet>
         <meta charSet="utf-8" />
         <title>Hermann Blum</title>
-        <link
-          rel="icon"
-          sizes="any"
-          type="image/svg+xml"
-          href="/favicon.svg"
-        />
+        <link rel="icon" sizes="any" type="image/svg+xml" href="/favicon.svg" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
       </Helmet>
       <div class="columns">
         <div class="column is-6 is-offset-3 is-desktop">
@@ -32,12 +26,53 @@ export default function Layout({ children }) {
               }}
               dangerouslySetInnerHTML={{ __html: icons.pushbot }}
             ></span>
-            <a className="navbar-item">Home</a>
+            <a className="navbar-item" href="/">
+              Home
+            </a>
           </nav>
         </div>
       </div>
       <div class="columns">
-        <div class="column is-6 is-offset-3 is-size-7">
+        <div
+          class="decoration"
+          style={{
+            height: "10rem",
+            width: "5rem",
+            top: "20rem",
+            left: "7%",
+            transform: "rotate(-11deg)",
+          }}
+        >
+          <img src="/robo3.svg" alt=""></img>
+        </div>
+        <div
+          class="decoration"
+          style={{
+            height: "10rem",
+            width: "7rem",
+            top: "1rem",
+            left: "80%",
+            //transform: "rotate(7deg)",
+          }}
+        >
+          <img src="/robo2.svg" alt=""></img>
+        </div>
+        <div
+          class="decoration"
+          style={{
+            height: "10rem",
+            width: "4rem",
+            top: "15rem",
+            left: "calc(80% + 5rem)",
+            transform: "rotate(7deg)",
+          }}
+        >
+          <img src="/robo1.svg" alt=""></img>
+        </div>
+        <div class="column is-2 is-offset-3">
+          <img src="/profile.svg" alt="profile picture"></img>
+        </div>
+        <div class="column is-4 is-size-7">
           <p>
             Hi, I'm Hermann. I work as a PhD Student at the Autonomous Systems
             Lab of ETH Z&uuml;rich.
@@ -48,7 +83,11 @@ export default function Layout({ children }) {
             geometrically, in order to perform manipulation and other safety
             critical tasks in proximity to humans.
           </p>
-          <p class="mt-2">
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column is-7 is-offset-3 is-size-7">
+          <p>
             <span
               class="icon pr-2"
               dangerouslySetInnerHTML={{ __html: icons.github }}
@@ -64,11 +103,21 @@ export default function Layout({ children }) {
           </p>
           <p class="mt-2">
             Visit the <a href="https://asl.ethz.ch">ASL website</a> for latest
-            updates from my colleagues and supervisors
+            updates from my colleagues and supervisors.
           </p>
+          <p class="mt-2">Here are my recent works:</p>
         </div>
       </div>
       {children}
+      <div class="columns">
+        <div class="column is-7 is-offset-3 is-size-7">
+          <p>
+            Thanks to&nbsp;
+            <a href="http://www.freepik.com">0melapics / Freepik</a>
+            &nbsp;for the robot decorations.
+          </p>
+        </div>
+      </div>
     </>
   )
 }
